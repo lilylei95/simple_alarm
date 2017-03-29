076aa22e22238a81e8851b4b9dc37aa8fbd2fcd0
lilylei95 <lilylei95@hotmail.com>
2017-03-29 11:48:56 -0700

    Made folder to cointain code

---
+++ Code/Source code/Main.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <ctime>
+#include "alarm.h"
+using namespace std;
+
+int main(){
+
+alarm MainAlarm;
+MainAlarm.run();
+
+
+return 0;
+} 
